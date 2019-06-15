cc82499b772db98ef08947f5dc90466ba6585407
GaJin <gajin2000@gmail.com>
2019-06-15 18:59:59 -0500

    ID Update

--- AreologyBot/run.py
+++ AreologyBot/run.py
@@ -18,17 +18,15 @@ if __name__ == "__main__":
         print ("Starting a local game ")
         random_map = random.choice(["BlueShiftLE", "CeruleanFallLE", "KairosJunctionLE", "ParaSiteLE", "PortAleksanderLE", "StasisLE", "CyberForestLE", "KingsCoveLE", "NewRepugnancyLE", "YearZeroLE"])
 
-        # Computer(race, difficulty, build)
         """
-        race:       [0] Random  | [1] Terran  | [2] Protoss | [3] Zerg     |
-        difficulty: [0] Easy    | [1] Medium  | [2] Hard    | [3] V Hard   | [4] Cheat Vision | [5] Cheat Money | [6] Cheat Insane  |
-        build:      [0] Random  | [1] Rush    | [2] Timing  | [3] Power    | [4] Macro        | [5] Air         |
+        race:       [0] Random  | [1] Terran    | [2] Protoss   | [3] Zerg      |                   |                   |                   |
+        difficulty: [0] Easy    | [1] Medium    | [2] Hard      | [3] V Hard    | [4] Cheat Vision  | [5] Cheat Money   | [6] Cheat Insane  |
+        build:      [0] Random  | [1] Rush      | [2] Timing    | [3] Power     | [4] Macro         | [5] Air           |                   |
         """
-        race = [Race.Random, Race.Terran, Race.Protoss, Race.Zerg]
-        difficulty = [Difficulty.Easy, Difficulty.Medium, Difficulty.Hard, Difficulty.VeryHard, Difficulty.CheatVision, Difficulty.CheatMoney, Difficulty.CheatInsane]
-        build = [sc2.AIBuild.RandomBuild, sc2.AIBuild.Rush, sc2.AIBuild.Timing, sc2.AIBuild.Power, sc2.AIBuild.Macro, sc2.AIBuild.Air]
-
-        enemy_bot = sc2.player.Computer(race[0], difficulty[6], build[0])
+        race =          [Race.Random,Race.Terran, Race.Protoss, Race.Zerg]
+        difficulty =    [Difficulty.Easy, Difficulty.Medium, Difficulty.Hard, Difficulty.VeryHard, Difficulty.CheatVision, Difficulty.CheatMoney, Difficulty.CheatInsane]
+        build =         [sc2.AIBuild.RandomBuild, sc2.AIBuild.Rush, sc2.AIBuild.Timing, sc2.AIBuild.Power, sc2.AIBuild.Macro, sc2.AIBuild.Air]
+        enemy_bot =     sc2.player.Computer(race[0], difficulty[6], build[0])   # Computer(race, difficulty, build)
 
         sc2.run_game(sc2.maps.get(random_map), [my_bot, enemy_bot],
         realtime = False, save_replay_as="AreologyBotReplay.SC2Replay")
