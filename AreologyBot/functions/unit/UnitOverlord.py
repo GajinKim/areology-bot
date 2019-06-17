7e501a1967d4c2b094c576a5a266f1b760356600
GaJin <gajin2000@gmail.com>
2019-06-17 16:31:45 -0500

    Updated scouting and retreating methods

--- AreologyBot/functions/unit/UnitOverlord.py
+++ AreologyBot/functions/unit/UnitOverlord.py
@@ -2,11 +2,11 @@ import sc2
 from sc2.ids.unit_typeid import UnitTypeId as UnitID
 
 class UnitOverlord:
-    async def sendScout(self):
+    async def sendEarlyGameScout(self):
         scouting_overlord = self.overlords[0]
         self.actions.append(scouting_overlord.move(self.enemy_start_locations[0]))
 
-    async def retreatScout(self):
+    async def retreatEarlyGameScout(self):
         for overlord in self.units(UnitID.OVERLORD):
             damaged_overlord = overlord.health < 200
             if damaged_overlord:
