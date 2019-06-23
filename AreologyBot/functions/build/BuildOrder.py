11daf30fa7646be5446316de383faa504d3f9a21
GaJin <gajin2000@gmail.com>
2019-06-23 17:56:27 -0500

    Finalized BuildOrder constructor organization for now

--- AreologyBot/functions/build/BuildOrder.py
+++ AreologyBot/functions/build/BuildOrder.py
@@ -11,18 +11,18 @@ class BuildOrder:
         # no point in executing function if you have less than 25 minerals
         if self.minerals < 25:
             return
-
         current_step = self.buildorder[self.buildorder_step]
-        # do nothing if we are done already or dont have enough resources for current step of build order
+        # check that we're in the correct phase and we have enough resources
         if current_step == "ALLIN PHASE" or current_step == "MACRO PHASE" or not self.can_afford(current_step):
             return
-        # check if current step needs larva
+        # steps that consume larva
         if current_step in self.from_larva and self.larvae:
-            self.actions.append(self.larvae.first.train(current_step))
-            print(f"{self.time_formatted} STEP {self.buildorder_step} {current_step.name} ")
-            self.buildorder_step += 1
-        elif current_step == UnitID.EXTRACTOR or current_step == UnitID.HATCHERY:
+            await BuildStep.stepLarvaUnit(self)
+        # steps that don't consume larva
+        elif current_step == UnitID.EXTRACTOR:
             await BuildStep.stepExtractor(self)
+        elif current_step == UnitID.HATCHERY:
+            await BuildStep.stepHatchery(self)
         elif current_step == UnitID.QUEEN:
             await BuildStep.stepQueen(self)
         elif current_step == AbilID.RESEARCH_ZERGLINGMETABOLICBOOST:
--- AreologyBot/functions/build/BuildStep.py
+++ AreologyBot/functions/build/BuildStep.py
@@ -3,6 +3,15 @@ from sc2.ids.ability_id import AbilityId as AbilID
 from sc2.ids.unit_typeid import UnitTypeId as UnitID
 
 class BuildStep:
+
+    async def stepLarvaUnit(self):
+        current_step = self.buildorder[self.buildorder_step]
+
+        self.actions.append(self.larvae.first.train(current_step))
+        print(f"{self.time_formatted} STEP {self.buildorder_step} {current_step.name} ")
+        self.buildorder_step += 1
+
+
     async def stepQueen(self):
         current_step = self.buildorder[self.buildorder_step]
         # tech requirement check
@@ -24,10 +33,16 @@ class BuildStep:
         self.buildorder_step += 1
 
     async def stepExtractor(self):
+        current_step = self.buildorder[self.buildorder_step]
+
         if current_step == UnitID.EXTRACTOR:
             # get geysers that dont have extractor on them
             geysers = self.state.vespene_geyser.filter(lambda g: all(g.position != e.position for e in self.units(UnitID.EXTRACTOR)))
             position = geysers.closest_to(self.start_location)
+
+    async def stepHatchery(self):
+        current_step = self.buildorder[self.buildorder_step]
+
         if current_step == UnitID.HATCHERY:
             position = await self.get_next_expansion()
         # closest worker
